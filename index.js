a5efa512529de6fb6fd759dce9952330a5679ce0
Mohd Najamuddin <mohdnajmuddin8542@gmail.com>
2024-08-01 00:43:04 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+// script for header foooter
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('assets/header.html').then(Response=> Response.text())
+    .then(data=>{
+        document.getElementById('header-content').innerHTML=data;
+    });
+
+    fetch('assets/footer.html').then(Response => Response.text())
+    .then(data => {
+        document.getElementById('footer-content').innerHTML=data;
+    });
+});
+
+
+
 const bar=document.getElementById("bar");
 const navbar=document.getElementById("navbar");
 
 if(bar){
     bar.addEventListener('click',()=>{
         navbar.classList.add('active');
-    })
+    });
 }
 
 const close=document.getElementById("close");
@@ -12,5 +28,17 @@ if(close)
 {
     close.addEventListener('click',()=>{
         navbar.classList.remove('active');
-    })
+    });
+}
+ 
+
+// singel_product script
+
+const main_img=document.getElementById("main-image");
+const small_img=document.getElementsByClassName("small-image");
+
+for(let i=0; i<small_img.length;i++){
+small_img[i].onclick=function() {
+    main_img.src=small_img[i].src;
+}
 }
